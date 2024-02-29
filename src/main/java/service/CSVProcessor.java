912b4834a1dec98fd768aa994e64f8e1da58f2a2
ananya <ananya.sahu0894@gmail.com>
2024-02-29 12:59:43 +0000

    updating file path

--- src/main/java/service/CSVProcessor.java
+++ src/main/java/service/CSVProcessor.java
@@ -11,8 +11,8 @@ import java.util.List;
 import java.util.Objects;
 
 public class CSVProcessor {
-     String csvDataFilePath = "resources/data.csv";
-     String newCSVDataFilePath = "resources/processedCSV.csv";
+     String csvDataFilePath = "../../resources/data.csv";
+     String newCSVDataFilePath = "../../resources/processedCSV.csv";
      int countValidRecords = 0;
 
 
